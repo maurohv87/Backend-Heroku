5e22d969a741c7850e40052f0d4cb3ef2503824b
maurohv87 <maurohv_87@hotmail.com>
2022-10-18 22:42:38 -0300

    conexion con base de datos

---
+++ mauv/src/main/java/com/mauportfolio/mauv/MauvApplication.java
@@ -0,0 +1,13 @@
+package com.mauportfolio.mauv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MauvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MauvApplication.class, args);
+	}
+
+}
---
+++ mauv/src/test/java/com/mauportfolio/mauv/MauvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauportfolio.mauv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MauvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
